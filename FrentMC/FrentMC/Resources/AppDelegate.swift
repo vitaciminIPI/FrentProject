f885ba56a9a7a50455e4ddfc2fcc0594665f44a2
ismawan <ismawanmaulidza@gmail.com>
2022-09-29 14:16:09 +0900

    Update firebase

--- FrentMC/FrentMC/Resources/AppDelegate.swift
+++ FrentMC/FrentMC/Resources/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,10 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        FirebaseApp.configure()
+        
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = TabBarViewController()
         window.makeKeyAndVisible()
         self.window = window
+        
         
         return true
     }
